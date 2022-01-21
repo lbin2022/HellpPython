7c09e2fe272a0d257c82595a3a005042c1ed339d
Edward9999 <36750891+Edward9999@users.noreply.github.com>
2022-01-21 13:47:55 +0800

    提交 0003 无重复字符的最长子串(解法2）

--- leetcode_china/0000-0100/0003 无重复字符的最长子串.py
+++ leetcode_china/0000-0100/0003 无重复字符的最长子串.py
@@ -8,7 +8,29 @@ import datetime
 
 class Solution:
     @staticmethod
-    def lengthOfLongestSubstring(s: str) -> int:
+    def lengthOfLongestSubstring1(s: str) -> int:
+        """
+        我的答案：列表实现
+        :param s: 字符串
+        :return: 不重复子串长度
+        """
+        str_result = str_cur = s[0]   # 结果字符串, 当前子串
+        str_max = len(s)
+        result_max = len(set(s))    # 可能的最大结果
+        if result_max == str_max:   # 无重复，直接返回s
+            str_result = s
+        else:
+            for i in range(1, str_max):
+                if str_cur.find(s[i]) < 0:
+                    str_cur += s[i]
+                    if len(str_result) < len(str_cur) <= result_max:
+                        str_result = str_cur
+                else:
+                    str_cur = str_cur[1:] + s[i]
+        return len(str_result)
+
+    @staticmethod
+    def lengthOfLongestSubstring0(s: str) -> int:
         """
         我的答案：列表实现
             986 / 987 个通过测试用例
@@ -22,12 +44,12 @@ class Solution:
                 sub_set = set(sub)
                 if len(sub) == len(sub_set):
                     return l_sub
-
         return 0
 
 
-st = '''abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ '''
+st = '''"pwwkew"'''
 start = datetime.datetime.now()
-print(Solution.lengthOfLongestSubstring(st))
+print(Solution.lengthOfLongestSubstring1(st))
+print(Solution.lengthOfLongestSubstring0(st))
 end = datetime.datetime.now()
 print('耗时', end - start)
